b869d2406c15dc3c4089ea7bd97dbcbcf7b6e701
SuhyungK <dook4966@gmail.com>
2023-10-31 11:48:17 +0900

    feat: 레포지토리 목록 구현

--- github-finder/script.js
+++ github-finder/script.js
@@ -5,14 +5,19 @@ inputEl.addEventListener('keydown', (e) => searchUser(e))
 
 const loader = document.querySelector('.loader')
 
+// get section info
+const section = document.getElementById('section')
+
 async function searchUser(e) {
-    let userInfo = null
+    let userInfo = null 
+
     if (e.code == 'Enter') {
         loader.style.display = 'block'
         getUserInfo(e.target.value)
             .then((res) => {
                 loader.style.display = 'none'
                 drawPage(res)
+                drawRepo(res.repoList)
             })
             .catch((err) => {
                 console.log(err)
@@ -53,9 +58,6 @@ async function getUserInfo(value) {
 
 function drawPage(userInfo) {
 
-    // get section info
-    const section = document.getElementById('section')
-
     // create element + add attributes
     const infoArticle = document.createElement('article')
     infoArticle.setAttribute('id', 'github_info')
@@ -77,19 +79,29 @@ function drawPage(userInfo) {
     const infoLabelDiv = document.createElement('div')
     const blueLabel = document.createElement('div')
     blueLabel.classList.add('blue_box', 'box')
-    blueLabel.innerHTML = 'Public Repos'
+    blueLabel.innerHTML = 'Public Repos : ' + userInfo.public_repos
     const greyLabel = document.createElement('div')
     greyLabel.classList.add('grey_box', 'box')
-    greyLabel.innerHTML = 'Public Gists'
+    greyLabel.innerHTML = 'Public Gists : ' + userInfo.public_gists
     const greenLabel = document.createElement('div')
     greenLabel.classList.add('green_box', 'box')
-    greenLabel.innerHTML = 'Followers'
+    greenLabel.innerHTML = 'Followers : ' + userInfo.followers
     const turquoiseLabel = document.createElement('div')
     turquoiseLabel.classList.add('turquoise_box', 'box')
-    turquoiseLabel.innerHTML = 'Following'
+    turquoiseLabel.innerHTML = 'Following : ' + userInfo.following
     
-    // repo
-
+    const tableEl = document.createElement('table')
+    const row1 = tableEl.insertRow(0)
+    row1.innerHTML = 'Company : ' + userInfo.company 
+    const row2 = tableEl.insertRow(1)
+    row2.innerHTML = 'Website/Blog : ' + userInfo.blog
+    const row3 = tableEl.insertRow(2)
+    row3.innerHTML = 'Location : ' + userInfo.location 
+    const row4 = tableEl.insertRow(3)
+    row4.innerHTML = 'Member Since : ' + userInfo.created_at 
+    tableEl.classList.add('info_table')
+    // repo 
+    
     // change style
     leftDiv.style.width = '30%'
     leftDiv.style.textAlign = 'center'
@@ -99,7 +111,6 @@ function drawPage(userInfo) {
     // leftDiv.style.alignItems = 'center'
     
     rightDiv.style.width = '70%'
-
 
     // append HTML
     leftDiv.append(imgDiv)
@@ -111,11 +122,52 @@ function drawPage(userInfo) {
     infoLabelDiv.append(turquoiseLabel)
     
     rightDiv.append(infoLabelDiv)
+    rightDiv.appendChild(tableEl)
 
     infoArticle.append(leftDiv)
     infoArticle.append(rightDiv)
 
     section.append(infoArticle)
+}
+
+function drawRepo(reposList) {
+    const subTitle = document.createElement('p')
+    subTitle.innerText = 'Latest Repository'
+    subTitle.classList.add('sub_title')
+
+    section.append(subTitle)
+
+    const repoListDiv = document.createElement('div')
+    for (const repo of reposList) {
+        const { name, stargazers_count, watchers_count, forks } = repo
+
+        const repoDiv = document.createElement('div')
+        repoDiv.style.display = 'flex'
+        repoDiv.classList.add('default_border', 'repo_div')
+        const repoTitle = document.createElement('p')
+        repoTitle.innerText = name
+        repoTitle.style.width = '60%'
+        const repoLabelList = document.createElement('div')
+        repoLabelList.style.display = 'flex'
+        const repoStarLabel = document.createElement('div')
+        repoStarLabel.innerText = 'Stars: ' + stargazers_count
+        repoStarLabel.classList.add('blue_box', 'box')
+        const repoWatcherLabel = document.createElement('div')
+        repoWatcherLabel.innerText = 'Watchers: ' + watchers_count
+        repoWatcherLabel.classList.add('grey_box', 'box')
+        const repoForkLabel = document.createElement('div')
+        repoForkLabel.innerText = 'Forks: ' + forks
+        repoForkLabel.classList.add('green_box', 'box')
+        
+        repoLabelList.append(repoStarLabel)
+        repoLabelList.append(repoWatcherLabel)
+        repoLabelList.append(repoForkLabel)
+        repoDiv.append(repoTitle)
+        repoDiv.append(repoLabelList)
+        repoListDiv.append(repoDiv)
+    }
+
+    section.appendChild(repoListDiv)
 }
 
 async function getApi(params=null, url=null) {
