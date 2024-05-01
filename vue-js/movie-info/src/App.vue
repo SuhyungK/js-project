82cd435ff348a0fd5011a1b35b6dc10347d1a2a2
suhyungk <dook4966@gmail.com>
2024-05-02 00:41:50 +0900

    반복문으로 영화 정보, click 이벤트 좋아요 버튼

--- vue-js/movie-info/src/App.vue
+++ vue-js/movie-info/src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <h1>영화 정보</h1>
-  <div>
-    <h3 class="bg-yellow" :style="textRed">{{ title }}</h3>
-    <p>개봉: {{ year }}</p>
-    <p>장르: {{ category }}</p>
+  <div v-for="(movie, i) in data" :key="i">
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <p>개봉: {{ movie.year }}</p>
+    <p>장르: {{ movie.category }}</p>
+    <button @:click="increaseLike(i)">좋아요</button>
+    <span> {{ movie.like }}</span>
   </div>
-
-  <p v-for="(item, i) in foods" :key="i">
-    {{ item }}
-  </p>
 </template>
 
 <script>
@@ -16,12 +14,33 @@ export default {
   name: "App",
   data() {
     return {
-      foods: ["김밥", "순대", "만두"],
-      title: "노량",
-      year: 2023,
-      category: "액션, 드라마",
+      data: [
+        {
+          title: "노량",
+          year: 2023,
+          category: "액션, 드라마",
+          like: 0,
+        },
+        {
+          title: "아쿠아맨과 로스트 킹덤",
+          year: 2023,
+          category: "액션, 판타지, 어드벤처",
+          like: 0,
+        },
+        {
+          title: "3일의 휴가",
+          year: 2023,
+          category: "판타지, 드라마",
+          like: 0,
+        },
+      ],
       textRed: "color: red",
     };
+  },
+  methods: {
+    increaseLike(i) {
+      this.data[i].like += 1;
+    },
   },
 };
 </script>
