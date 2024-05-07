8d5434118cddcfa5484b6d865e14a63c7e25bb8f
suhyungk <dook4966@gmail.com>
2024-05-07 20:26:18 +0900

    composition api 연습

---
+++ vue-js/my-project/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { onMounted, ref, watch } from "vue";
+import ChildComp from "./components/ChildComp.vue";
+
+let count = ref(0); // 상태변수 초기값(state)
+let title = "Hello Vue";
+let color = ref("green");
+
+onMounted(() => {
+  console.log("mounted");
+});
+
+const increaseCount = () => {
+  count.value++;
+};
+
+watch(color, () => {
+  console.log("color 변경");
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="increaseCount">count++</button>
+    <p>{{ count }}</p>
+    <ChildComp :color="color" bgColor="gold" />
+    <button @click="color = 'red'">change color</button>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue-js/my-project/src/components/ChildComp.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p :style="{ color: color, backgroundColor: bgColor }">{{ color }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: String,
+  bgColor: String,
+});
+</script>
+<style lang=""></style>
