654c994673a383b327aebc6ac156e5a3ebeb0200
suhyungk <dook4966@gmail.com>
2024-06-08 23:51:45 +0900

    todolist UI 구현하기

---
+++ react-study/todo-list/src/components/List.jsx
@@ -0,0 +1,19 @@
+import "./List.css";
+
+import TodoItem from "./TodoItem";
+
+const List = () => {
+  return (
+    <div className="List">
+      <h3>Todo List 🌱</h3>
+      <input type="text" placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default List;
