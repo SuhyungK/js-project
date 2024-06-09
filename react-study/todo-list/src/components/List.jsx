20cd0c6538d9d6e256d22a660cdad4db7385383e
suhyungk <dook4966@gmail.com>
2024-06-09 15:27:41 +0900

    todo 리스트 랜더링 & 검색기능

--- react-study/todo-list/src/components/List.jsx
+++ react-study/todo-list/src/components/List.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import "./List.css";
 
 import TodoItem from "./TodoItem";
 
 const List = ({ todos }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const onSearchChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (keyword === "") return todos;
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(keyword.toLowerCase())
+    );
+  };
+
+  const filteredData = getFilteredData();
+
   return (
     <div className="List">
       <h3>Todo List 🌱</h3>
-      <input type="text" placeholder="검색어를 입력하세요" />
+      <input
+        onChange={onSearchChange}
+        type="text"
+        placeholder="검색어를 입력하세요"
+      />
       <div className="todos_wrapper">
-        {todos
-          ? todos.map((todo) => {
-              return <div key={todo.id}>{todo.content}</div>;
+        {filteredData
+          ? filteredData.map((todo) => {
+              return <TodoItem key={todo.id} {...todo} />;
             })
           : null}
       </div>
