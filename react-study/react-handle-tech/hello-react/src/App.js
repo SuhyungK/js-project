5788e18646455905ed920d78dff7d3202ef79eb9
suhyungk <dook4966@gmail.com>
2024-08-14 11:01:27 +0900

    7장 라이프사이클

--- react-study/react-handle-tech/hello-react/src/App.js
+++ react-study/react-handle-tech/hello-react/src/App.js
@@ -1,12 +1,30 @@
 import { Component } from 'react';
 import './App.css';
-import IterationSample from './components/IterationSample';
+import ErrorBoundary from './components/LifeCycleSample';
+import LifeCycleSample from './components/LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
