a0ad79d581ada2b88111831852cd51da2a4c09f3
suhyungk <dook4966@gmail.com>
2024-08-16 22:12:49 +0900

    11장 성능 최적화

--- react-study/todo-app/src/App.js
+++ react-study/todo-app/src/App.js
@@ -4,55 +4,52 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 0; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: i % 3 === 0 ? true : false,
+    });
+  }
+
+  return array;
+}
+
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '할 일 1',
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback((value) => {
+    const newTodo = {
+      id: nextId,
+      text: value,
       checked: true,
-    },
-    {
-      id: 2,
-      text: '할 일 2',
-      checked: false,
-    },
-    {
-      id: 3,
-      text: '할 일 3',
-      checked: false,
-    },
-  ]);
+    };
+    setTodos((todos) => todos.concat(newTodo));
+    nextId.current += 1;
+  }, []);
 
-  const nextId = useRef(4);
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onInsert = useCallback(
-    (value) => {
-      const newTodo = {
-        id: nextId,
-        text: value,
-        checked: true,
-      };
-      setTodos(todos.concat(newTodo));
-      nextId.current += 1;
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, checked: !todo.checked };
+          } else {
+            return todo;
+          }
+        }),
+      );
     },
     [todos],
   );
-
-  const onRemove = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
-  const onToggle = (id) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, checked: !todo.checked };
-        } else {
-          return todo;
-        }
-      }),
-    );
-  };
 
   return (
     <TodoTemplate>
