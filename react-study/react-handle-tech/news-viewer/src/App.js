9f7cf0efe4361e1bbdc873a3159e6e8ee1374377
suhyungk <dook4966@gmail.com>
2024-08-21 13:14:10 +0900

    14장 axios 사용하기

--- react-study/react-handle-tech/news-viewer/src/App.js
+++ react-study/react-handle-tech/news-viewer/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from '../node_modules/axios/index';
 
 function App() {
+  const [data, setData] = useState(null);
+  // const onClick = () => {
+  //   axios
+  //     .get('https://jsonplaceholder.typicode.com/todos/1')
+  //     .then((response) => {
+  //       console.log(response);
+  //       setData(response.data);
+  //     });
+  // };
+
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=ccb79b13da5f4b1184009049594cf50e',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
 }
