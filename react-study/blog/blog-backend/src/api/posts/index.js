e5fc10b090a847406b3fc3aac461d4862f524ed8
suhyungk <dook4966@gmail.com>
2024-08-27 23:57:15 +0900

    node.js 백엔드 서버 설정

---
+++ react-study/blog/blog-backend/src/api/posts/index.js
@@ -0,0 +1,12 @@
+const Router = require('koa-router');
+const posts = new Router();
+
+const printInfo = (ctx) => {
+  ctx.body = {
+    method: ctx.method,
+    path: ctx.path,
+    params: ctx.params,
+  };
+};
+
+module.exports = posts;
